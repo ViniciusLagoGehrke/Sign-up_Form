8d2cd66a40a0b6eb1f06e532ff313905302c0ae4
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-09-11 15:51:37 +0000

    refactored form validation

--- js/form.js
+++ js/form.js
@@ -1,83 +1,53 @@
 const form = document.getElementById('form');
- 
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
 const email = document.getElementById('emailAddress');
 const password = document.getElementById('password');
- 
-const btn = document.querySelector('.button');
- 
-const errorFirstName = document.querySelector('.firstName');
-const errorLastName = document.querySelector('.lastName');
-const errorEmail = document.querySelector('.emailAddress');
-const errorPassword = document.querySelector('.password');
- 
+
+// refactored //
 form.addEventListener('submit', (e) => {
  e.preventDefault();
  
- checkInputs();
+ checkInputs([firstName, lastName, email, password]);
+ checkEmail(email);
 });
- 
-function checkInputs(){
- //get the values from inputs
- const firsNameValue = firstName.value.trim(); //trim removes unwanted spaces
- const lastNameValue = lastName.value.trim();
- const emailValue = email.value.trim();
- const passwordValue = password.value.trim();
- 
- if(firsNameValue === '' || firsNameValue == null){
-   //show error
-   //add error class
-   setErrorFor(firstName, 'First Name cannot be empty');
- } else {
-   // submit
-   setSuccessFor(firstName);
- }
- 
- if(lastNameValue === ''|| lastNameValue == null){
-   //show error
-   //add error class
-   setErrorFor(lastName, 'Last Name cannot be empty');
- } else {
-   //set Success
-   setErrorFor(lastName, 'Last Name cannot be empty');
-   setSuccessFor(lastName);   
- }
- 
- if(emailValue === '' || emailValue == null){
-   setErrorFor(email, 'Email address cannot be empty');
- } else if(!isEmail(emailValue)){
-   setErrorFor(email,'Looks like this in not an email');
- } else {
-   setSuccessFor(email);
- }
- 
- if(passwordValue === ''|| passwordValue == null){
-   //show error
-   //add error class
-   setErrorFor(password, 'Password cannot be empty');
- } else {
-       setSuccessFor(email);
- }
+
+// refactored //
+function checkInputs(inputArray){
+ inputArray.forEach((input) => {
+   if(input.value.trim() === "" || input.value.trim == null){
+     setError(input, `${input.placeholder} cannot be empty`);
+   } else {
+     setSuccess(input);
+   }
+ });
 }
- 
-function setErrorFor(input, message){
+
+// refactored //
+function setError(input, message){
  const formControl = input.parentElement; //div .form-control
  const errorMessage = formControl.querySelector('.error-msg');
  
  //add error message inside small
  errorMessage.innerText = message;
  
- //add error class
+ //add error class on input
  formControl.className = 'form-control error';
 }
- 
-function setSuccessFor(input) {
+
+// refactored //
+function setSuccess(input) {
  const formControl = input.parentElement;
  formControl.className = "form-control";
 }
- 
-function isEmail(email){
+
+// NEEDS TO BE refactored //
+function checkEmail(email){
  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- return re.test(String(email).toLowerCase());
+ if(re.test(String(email.value).trim())){
+  email.style.color = "hsl(249, 31%, 18%)";
+  } else {
+    setError(input, "Looks like this is not an email");
+    email.style.color = "hsl(0, 100%, 74%)";
+  }
 }
